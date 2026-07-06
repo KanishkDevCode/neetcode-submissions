ed104b261505d5a744b9c9c4820fe79a7b45905c
KanishkDevCode <kanishkdevcode@gmail.com>
2026-07-06 20:32:27 +0530

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.cpp
@@ -0,0 +1,52 @@
+class MyQueue {
+
+    stack<int> in, out;
+
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        
+        in.push(x);
+    }
+    
+    int pop() {
+        
+        if(out.empty()) {
+
+            while(!in.empty()) {
+
+                out.push(in.top());
+
+                in.pop();
+            }
+        }
+
+        int x = out.top();
+
+        out.pop();
+
+        return x;
+    }
+    
+    int peek() {
+        
+        if(out.empty()) {
+
+            while(!in.empty()) {
+
+                out.push(in.top());
+
+                in.pop();
+            }
+        }
+        return out.top();
+    }
+    
+    bool empty() {
+        
+        return in.empty() && out.empty();
+    }
+};
