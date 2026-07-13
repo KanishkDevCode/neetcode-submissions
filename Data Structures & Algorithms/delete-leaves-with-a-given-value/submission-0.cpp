c6a49048ef2744d1a5ef2ef68785ab699f8b9349
Kanishk <kanishkdevcode@gmail.com>
2026-07-13 23:00:33 +0530

    Add: delete-leaves-with-a-given-value - submission-0

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        
+        if(root == NULL)
+            return NULL;
+
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+
+        if(root->left == NULL &&
+           root->right == NULL &&
+           root->val == target)
+        {
+            delete root;
+            return NULL;
+        }
+
+        return root;
+    }
+};
