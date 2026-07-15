d554337eebd7596b4e0eb05eb9a0300ab4901cfd
Kanishk <kanishkdevcode@gmail.com>
2026-07-15 23:20:57 +0530

    Add: ipo - submission-0

---
+++ Data Structures & Algorithms/ipo/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w,
+                             vector<int>& profits,
+                             vector<int>& capital) {
+
+        priority_queue<pair<int,int>,
+                       vector<pair<int,int>>,
+                       greater<pair<int,int>>> minCapital;
+
+        priority_queue<int> maxProfit;
+
+        for(int i=0;i<profits.size();i++)
+            minCapital.push({capital[i],profits[i]});
+
+        while(k--)
+        {
+            while(!minCapital.empty() &&
+                  minCapital.top().first<=w)
+            {
+                maxProfit.push(minCapital.top().second);
+                minCapital.pop();
+            }
+
+            if(maxProfit.empty())
+                break;
+
+            w+=maxProfit.top();
+            maxProfit.pop();
+        }
+
+        return w;
+    }
+};
