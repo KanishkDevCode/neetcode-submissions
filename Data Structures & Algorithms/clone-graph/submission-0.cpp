3b627c5e7fa09c49636c44d9481992a36dde5fab
Kanishk <kanishkdevcode@gmail.com>
2026-07-17 17:30:41 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    unordered_map<Node*, Node*> mp;
+
+    Node* dfs(Node* node) {
+        
+        if (mp.count(node))
+            return mp[node];
+
+        Node* clone = new Node(node->val);
+        mp[node] = clone;
+
+        for (Node* nei : node->neighbors)
+            clone->neighbors.push_back(dfs(nei));
+
+        return clone;
+    }
+    Node* cloneGraph(Node* node) {
+        
+        if (node == NULL)
+            return NULL;
+
+        return dfs(node);
+    }
+};
